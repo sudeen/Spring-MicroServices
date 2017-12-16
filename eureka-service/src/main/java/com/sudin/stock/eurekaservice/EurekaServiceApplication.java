c856194b7e3e44a9ecec8df9a25619b283ef773c
s-kiips <ranjitkarsudeen14@gmail.com>
2017-12-17 01:00:47 +0545

    instances registered with eureka

--- eureka-service/src/main/java/com/sudin/stock/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/sudin/stock/eurekaservice/EurekaServiceApplication.java
@@ -2,11 +2,13 @@ package com.sudin.stock.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
 }
